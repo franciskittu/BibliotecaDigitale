a8244fc13d6ad65c3ca64494f7d9bbc45a674655
marcodettorre <marcodettorre88@gmail.com>
2016-08-24 12:23:07 +0200

    aggiornamento template: risolto problema nav bar e scorrimento. risolto problema paging per lista opere in pubblicazione acquisizione

--- WebContent/template/js/biblioteca.js
+++ WebContent/template/js/biblioteca.js
@@ -338,14 +338,15 @@ function switchPage(page) {
 	switch(sezione){
 		case 1:
 			updateTable("tableopere",data);
+			updatePager("paging",page);
 			break;
 		case 2:
 			updateTable("tableopereacquisizione",data);
+			updatePager("paging1",page);
 	}
-	updateTable("tableopere",data);
 	//aggiornamento dei link di paginazione
 	//update the pager
-	updatePager("paging",page);
+	
 }
 
 //inizializzazione dello script
@@ -433,69 +434,26 @@ function controllausername(obj){
             };
             
             
-            function controllaNumeroPagina(obj){
-            	var id = document.getElementById("opera").value;
-                $.ajax({
-                url: 'UploadImmagine',
-                type: 'POST',
-                data : {numeroAJAX:obj.value, operaAJAX:id },
-                //data: 'numeroAJAX='+obj.value+'&operaAJAX='+id,
-                    success: function(data) {
-                                if (eval(data)){
-                                    
-                                    document.getElementById("usernamecheck").innerHTML ="<div id=\"status\"><span style=\"top:20px\" class=\"glyphicon glyphicon-remove form-control-feedback\" aria-hidden=\"true\"></span><span id=\"inputError2Status\" class=\"sr-only\">(error)</span></div><p class=\"help-block text-danger\"><ul role=\"alert\"><li>Username gia' presente, inserirne un altro</li></ul></p>";
-                                    
-                                }
-                                
-                                else {
-                                    
-                                    $("#status").remove();
-                                    document.getElementById("usernamecheck").innerHTML ="<div id=\"status\"><span style=\"top:20px\" class=\"glyphicon glyphicon-ok form-control-feedback\" aria-hidden=\"true\"></span><span id=\"inputSuccess2Status\" class=\"sr-only\">(success)</span></div>";
-                                    
-                                }
+function controllaNumeroPagina(obj){
+	var id = document.getElementById("opera").value;
+    $.ajax({
+    url: 'UploadImmagine',
+    type: 'POST',
+    data : {numeroAJAX:obj.value, operaAJAX:id },
+    //data: 'numeroAJAX='+obj.value+'&operaAJAX='+id,
+        success: function(data) {
+                    if (eval(data)){
+                        
+                        document.getElementById("usernamecheck").innerHTML ="<div id=\"status\"><span style=\"top:20px\" class=\"glyphicon glyphicon-remove form-control-feedback\" aria-hidden=\"true\"></span><span id=\"inputError2Status\" class=\"sr-only\">(error)</span></div><p class=\"help-block text-danger\"><ul role=\"alert\"><li>Username gia' presente, inserirne un altro</li></ul></p>";
+                        
                     }
-                });
-                };            
-            
-(function($) {
-    "use strict"; // Start of use strict
-
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
-
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
-
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
-
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
+                    
+                    else {
+                        
+                        $("#status").remove();
+                        document.getElementById("usernamecheck").innerHTML ="<div id=\"status\"><span style=\"top:20px\" class=\"glyphicon glyphicon-ok form-control-feedback\" aria-hidden=\"true\"></span><span id=\"inputSuccess2Status\" class=\"sr-only\">(success)</span></div>";
+                        
+                    }
         }
-    })
-
-    // Floating label headings for the contact form
-    $(function() {
-        $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-        }).on("focus", ".floating-label-form-group", function() {
-            $(this).addClass("floating-label-form-group-with-focus");
-        }).on("blur", ".floating-label-form-group", function() {
-            $(this).removeClass("floating-label-form-group-with-focus");
-        });
     });
-
-})(jQuery); // End of use strict
+}            
